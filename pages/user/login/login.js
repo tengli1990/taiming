40ab24f14b8a2b3bd6835293cb27734073c08d4b
liteng <liteng@kaikeba.com>
2022-01-15 18:55:31 +0800

    feat: 新增页面排版

--- pages/user/login/login.js
+++ pages/user/login/login.js
@@ -1,62 +1,27 @@
-// pages/user/login.js
 Page({
   data: {
-    title: "太铭生物"
+    title: "太铭生物",
+    fillMode: 'man', // auto
+    mobile: '15810660233',
+    sms: '',
+    username: '11',
+    agreementChecked: true
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  // 生命周期函数--监听页面加载
   onLoad: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  changeMode() {
+    this.setData({
+      fillMode: 'manual'
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 授权协议选中
+  onAgreementChange(e) {
+    const { agreementChecked } = this.data
+    this.setData({
+      agreementChecked: !agreementChecked
+    })
   }
 })
